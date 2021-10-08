0b179f861f50d01c242c72549da5a3f674bc4954
LeoBang <bluke8489@gmail.com>
2021-10-08 21:44:07 +0900

    solve: BOJ BruteForce 2309 일곱난쟁이

--- programmers_lv2/행렬테두리회전/행렬테두리회전.py
+++ programmers_lv2/행렬테두리회전/행렬테두리회전.py
@@ -1,21 +1,34 @@
-from typing import Collection
-
-
 def solution(rows, columns, queries):
     mat = matrix(rows, columns)
+    answer = []
 
     for query in queries:
-        temp1, temp2 = mat[query[0] - 1][query[1] - 1], mat[query[2] - 1][query[3] - 1]
-        print(temp1, temp2)
-    return mat
+        x, y, p, q = query
+        
+        inner_vertex = mat[x - 1][y - 1]
+        outer_vertex = mat[p - 1][q - 1]
+        minimum = min(inner_vertex, outer_vertex)
+
+        for i, j in zip(range(x, p), reversed(range(x, p))):
+            mat[i - 1][y - 1] = mat[i][y - 1]
+            mat[j][q - 1] = mat[j - 1][q - 1]
+            minimum = min(minimum, mat[i][y - 1], mat[j - 1][q - 1])
+        
+        for i, j in zip(range(y, q), reversed(range(y, q))):
+            if i == q - 1:
+                mat[p - 1][i - 1] = outer_vertex
+                mat[x - 1][j] = inner_vertex
+            else:
+                mat[p - 1][i - 1] = mat[p - 1][i]
+                mat[x - 1][j] = mat[x - 1][j - 1]
+                minimum = min(minimum, mat[p - 1][i],mat[x - 1][j - 1])
+        
+        answer.append(minimum)
+    
+    return answer
+                
 
 
 def matrix(rows, columns):
-    mat = [[i + j * 6 for i in range(1, columns + 1)] for j in range(rows)]
+    mat = [[i + j * columns for i in range(1, columns + 1)] for j in range(rows)]
     return mat
-
-
-x = 6
-y = 6
-q = [[2,2,5,4],[3,3,6,6],[5,1,6,3]]
-solution(x, y, q)
