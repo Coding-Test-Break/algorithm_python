388dc7a315e8f72ebae3245bd64d9276f214afcf
LeoBang <bluke8489@gmail.com>
2021-12-28 21:08:58 +0900

    solve: BOJ BruteForce 1620 나는야 포켓몬 마스터 이다솜

--- scratch.py
+++ scratch.py
@@ -1,20 +1,17 @@
 import sys
 
-N, r, c = map(int, sys.stdin.readline().split())
+N, M = map(int, sys.stdin.readline().split())
+pokemon = [0]
+pokemon_dict = dict()
 
-def recur(r, c, count, N): 
-  if N == 0:
-    return count
-  r_share, r_remainder = divmod(r, 2 ** (N - 1))
-  c_share, c_remainder = divmod(c, 2 ** (N - 1))
-  
-  if (r_share, c_share) == (0, 0):
-    return recur(r_remainder, c_remainder, count + 4 ** (N - 1) * 0, N - 1)
-  elif (r_share, c_share) == (0, 1):
-    return recur(r_remainder, c_remainder, count + 4 ** (N - 1) * 1, N - 1)
-  elif (r_share, c_share) == (1, 0):
-    return recur(r_remainder, c_remainder, count + 4 ** (N - 1) * 2, N - 1)
-  elif (r_share, c_share) == (1, 1):
-    return recur(r_remainder, c_remainder, count + 4 ** (N - 1) * 3, N - 1)
+for i in range(N):
+  pokemon.append(sys.stdin.readline().strip())
+  pokemon_dict[pokemon[-1]] = i + 1
 
-print(recur(r, c, 0, N))
+for _ in range(M):
+  q = sys.stdin.readline().strip()
+  try:
+    num_q = int(q)
+    print(pokemon[num_q])
+  except:
+    print(pokemon_dict[q])
