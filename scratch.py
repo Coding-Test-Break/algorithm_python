e9ca54cbeeef9d10b5d49e9d0658b441a1e8d25a
LeoBang <bluke8489@gmail.com>
2022-01-01 02:41:38 +0900

    add solution: BOJ BFS 2606 바이러스

--- scratch.py
+++ scratch.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 N = int(sys.stdin.readline().strip())
 M = int(sys.stdin.readline().strip())
@@ -10,17 +11,18 @@ for _ in range(M):
   node[x].append(y)
   node[y].append(x)
 
-count = 0
-visited = [0 for _ in range(N + 1)]
-def dfs(s):
-  global count
-  visited[s] = 1
-  for i in node[s]:
-    if not visited[i]:
-      count += 1 
-      dfs(i)
-  
-dfs(1)
-print(count)
+visited = [0 for i in range(N + 1)]
+visited[1] = 1
+def bfs(s):
+  q = deque(s)
+  count = 0
+  while q:
+    x = q.popleft()
+    for i in node[x]:
+      if not visited[i]:
+        q.append(i)
+        visited[i] = 1
+        count += 1
+  return count
 
-  
+print(bfs([1]))
