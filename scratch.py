8cd3025a777d10778a6d673005839cc14698a93e
LeoBang <bluke8489@gmail.com>
2022-02-17 12:31:18 +0900

    solve: BOJ 9465 스티커

--- scratch.py
+++ scratch.py
@@ -1,27 +1,22 @@
-from collections import deque
 import sys
 
+T = int(sys.stdin.readline())
 
-N, K = map(int, sys.stdin.readline().split())
+for _ in range(T):
+  n = int(sys.stdin.readline())  
+  sticker = list()
 
-# N -> K 까지의 최단거리를 저장하는 list
-dp = [False for _ in range(100001)]
-dp[N] = 0
+  for _ in range(2):
+    sticker.append(list(map(int, sys.stdin.readline().split()))) 
+  if n == 1:
+    print(max(sticker[0][0], sticker[1][0]))
+    continue
 
-def bfs():
-  # (node, 최단거리)를 저장하는 큐
-  q = deque()
-  q.append((N, 0))
-  
-  while q:
-    node, dist = q.popleft()
+  sticker[0][1] += sticker[1][0] 
+  sticker[1][1] += sticker[0][0] 
 
-    # node가 허용된 index안에 있고, 한번도 방문하지 않은 node이거나 방문했지만 최단거리가 비효율적인 경우 큐에 추가
-    for i, j in [(node + 1, dist + 1), (node - 1, dist + 1), (node * 2, dist)]:
-      if 0 <= i <= 100000 and (dp[i] is False or dp[i] > j):
-        dp[i] = j
-        q.append((i, j))
+  for j in range(2, n):
+    sticker[0][j] += max(sticker[1][j - 1], sticker[1][j - 2])
+    sticker[1][j] += max(sticker[0][j - 1], sticker[0][j - 2])
 
-bfs()
-
-print(dp[K])
+  print(max(sticker[0][-1], sticker[1][-1]))
